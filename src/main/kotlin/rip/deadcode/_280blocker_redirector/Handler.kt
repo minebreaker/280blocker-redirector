fb5ca3212b1290fe278c895867ee550c2630171a
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2021-02-17 22:27:37 +0900

    since cloudflare blocks automated requests, I decided not to check the page is there and just redirect.

--- src/main/kotlin/rip/deadcode/_280blocker_redirector/Handler.kt
+++ src/main/kotlin/rip/deadcode/_280blocker_redirector/Handler.kt
@@ -1,14 +1,8 @@
 package rip.deadcode._280blocker_redirector
 
-import com.google.api.client.http.GenericUrl
-import com.google.api.client.http.javanet.NetHttpTransport
-import com.google.common.io.CharStreams
 import com.google.common.net.HttpHeaders
 import org.eclipse.jetty.server.Request
 import org.eclipse.jetty.server.handler.AbstractHandler
-import org.slf4j.LoggerFactory
-import java.io.InputStreamReader
-import java.nio.charset.StandardCharsets
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import javax.servlet.http.HttpServletRequest
@@ -24,49 +18,13 @@ class Handler : AbstractHandler() {
     ) {
 
         val now = LocalDate.now().plusMonths(1)
-        val url = getUrl(now, 5)
+        val dateText = now.format(DateTimeFormatter.ofPattern("uuuuMM"))
+        val url = "https://280blocker.net/files/280blocker_adblock_${dateText}.txt"
 
         response.status = 307
         response.addHeader("Location", url)
 //        response.addHeader("Cache-Control", "max-age=604800")
         response.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
         baseRequest.isHandled = true
-    }
-
-    private val logger = LoggerFactory.getLogger(Handler::class.java)
-    private val httpTransport = NetHttpTransport()
-    private val formatter = DateTimeFormatter.ofPattern("uuuuMM")
-
-    fun getUrl(date: LocalDate, count: Int): String {
-        if (count == 0) {
-            throw RuntimeException("Too many failures.")
-        }
-
-        val dateText = date.format(formatter)
-
-        logger.info("Trying to check ${dateText}")
-        val url = "https://280blocker.net/files/280blocker_adblock_${dateText}.txt"
-        val response = httpTransport.createRequestFactory()
-            .buildGetRequest(GenericUrl(url))
-            .setThrowExceptionOnExecuteError(false)
-            .execute()
-
-        return when {
-            response.isSuccessStatusCode -> url
-            response.statusCode == 404 -> {
-                logger.info("404. Tries the previous month. Count: ${count}")
-                getUrl(date.minusMonths(1), count - 1)
-            }
-            else -> throw RuntimeException(
-                "Unexpected response: ${
-                    CharStreams.toString(
-                        InputStreamReader(
-                            response.content,
-                            StandardCharsets.UTF_8
-                        )
-                    )
-                }"
-            )
-        }
     }
 }
